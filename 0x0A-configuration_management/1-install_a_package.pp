ee071b91955b9a2485277dd9cc1f61d06e8f4268
imanewkm <imanewkm02@gmail.com>
2024-04-19 16:14:52 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-exec { 'install flask':
-  command => 'pip3 install flask==2.1.0',
-  unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
